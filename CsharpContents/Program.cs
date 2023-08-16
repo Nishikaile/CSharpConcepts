b3d8ce29192df725106d4c89dfc418dd4dca0740
nishilatha.kaile <nishilatha.kaile@AB-LPT-VJY-046>
2023-08-16 11:02:59 +0530

    switch cases

--- CsharpContents/Program.cs
+++ CsharpContents/Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CsharpContents.Trim;
+using CsharpContents.Switch;
 
 namespace CsharpContents
 {
@@ -14,28 +15,34 @@ namespace CsharpContents
     {
         static void Main(string[] args)
         {
-            var obj = new Class1();
-            obj.getData();
+            ////var obj = new Class1();
+            ////obj.getData();
 
-            Class2 obj2 = new Class2();
-            obj2.fun3();
-            Class1.SomeData();
-            obj2.evenodd(10);
-            obj2.evennumbers(10);
+            ////Class2 obj2 = new Class2();
+            ////obj2.fun3();
+            ////Class1.SomeData();
+            ////obj2.evenodd(10);
+            ////obj2.evennumbers(10);
 
-            var obj3 = new WithoutReturn();
-            obj3.fun4(3);
+            ////var obj3 = new WithoutReturn();
+            ////obj3.fun4(3);
 
-            ArthemeticOprts obj4 = new ArthemeticOprts();
-            Console.WriteLine("Multiplication: "+ArthemeticOprts.multiply(4,5));
+            ////ArthemeticOprts obj4 = new ArthemeticOprts();
+            ////Console.WriteLine("Multiplication: "+ArthemeticOprts.multiply(4,5));
 
-            int c=obj4.add(2, 3);
-            Console.WriteLine("Result of add fun:"+c);
+            ////int c=obj4.add(2, 3);
+            ////Console.WriteLine("Result of add fun:"+c);
 
 
-            UsingTrim obj5 = new UsingTrim();
-            obj5.trim("     Raghav","Raghav      ","Raghav");
+            ////UsingTrim obj5 = new UsingTrim();
+            ////obj5.trim("     Raghav","Raghav      ","Raghav");
+
+            UsingSwitch obj6 = new UsingSwitch();
+            obj6.athmoprts();
+
             Console.ReadKey();
+
+
         }
     }
 }
---
+++ CsharpContents/Switch/UsingSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpContents.Switch
+{
+    class UsingSwitch
+    {
+        public void athmoprts()
+        {
+            Console.WriteLine("enter value of a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter value of b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Select your choice from 1-5:");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            switch (c)
+            {
+                case 1:
+                    Console.WriteLine("Addition of " + a + " + " + b + " = " + (a + b));
+                    break;
+
+                case 2:
+                    Console.WriteLine("Subtraction of " + a + " - " + b + " = " + (a - b));
+                    break;
+
+                case 3:
+                    Console.WriteLine("Multiplication of " + a + " * " + b + " = " + (a * b));
+                    break;
+
+                case 4:
+                    Console.WriteLine("Division of " + a + " / " + b + " = " + (float)(a + b));
+                    break;
+
+                case 5:
+                    Console.WriteLine("Modulus of " + a + " % " + b + " = " + (a % b));
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+        }
+}
+}
